81cbb1fdc6aad79eda72e695e857a16b88a32f06
Sarp Eren EGILMEZ <2348.sarp.egilmez.2006@gmail.com>
2026-09-19 15:45:09 +0300

    fix: fine-tune offline dietpi settings and askpass environment

--- scripts/askpass.sh
+++ scripts/askpass.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-kdialog --password "${1:-Please enter your sudo password:}"
+export DISPLAY="${DISPLAY:-:0}"
+export WAYLAND_DISPLAY="${WAYLAND_DISPLAY:-wayland-0}"
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/1000}"
+exec /usr/bin/kdialog --password "${1:-Please enter your sudo password for Video Kiosk Build:}"
